9eb8a51d765e4bf636081c9ece24c934aa1d5475
Slawomir Johansen <baran@fastmail.com>
2022-05-18 03:24:56 +0200

    ADD error handling

--- src/components/WeatherBox/WeatherBox.js
+++ src/components/WeatherBox/WeatherBox.js
@@ -2,30 +2,44 @@ import PickCity from '../PickCity/PickCity';
 import WeatherSummary from '../WeatherSummary/WeatherSummary';
 import Loader from '../Loader/Loader';
 import { useCallback, useState } from 'react';
+import ErrorBox from '../ErrorBox/ErrorBox';
 
 const WeatherBox = () => {
   const [cityWeatherData, setCityWeatherData] = useState('');
+  const [pendingRequest, setPendingRequest] = useState(false);
+  const [requestError, setRequestError] = useState(false);
 
   const handleCityChange = useCallback((city) => {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=4e7a7169ccca411da2db9f35a8da82db&units=metric`)
-      .then((res) => res.json())
-      .then((data) => {
-        const weatherData = {
-          city: data.name,
-          temp: data.main.temp,
-          icon: data.weather[0].icon,
-          description: data.weather[0].main,
-        };
+    setRequestError(false);
+    setPendingRequest(true);
 
-        setCityWeatherData(weatherData);
-      });
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=4e7a7169ccca411da2db9f35a8da82db&units=metric`).then((res) => {
+      if (res.status === 200) {
+        return res.json().then((data) => {
+          const weatherData = {
+            city: data.name,
+            temp: data.main.temp,
+            icon: data.weather[0].icon,
+            description: data.weather[0].main,
+          };
+          setPendingRequest(false);
+
+          setCityWeatherData(weatherData);
+        });
+      } else {
+        setRequestError(true);
+        setPendingRequest(false);
+        setCityWeatherData('');
+      }
+    });
   }, []);
 
   return (
     <section>
       <PickCity action={handleCityChange} />
-      <WeatherSummary {...cityWeatherData } />
-      <Loader />
+      {cityWeatherData && <WeatherSummary {...cityWeatherData} />}
+      {pendingRequest && <Loader />}
+      {requestError && <ErrorBox />}
     </section>
   );
 };
